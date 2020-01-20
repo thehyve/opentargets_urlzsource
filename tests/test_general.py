7b8fd88683b34128f3fe564062d5fa7999c01768
Fedde Schaeffer <fedde@thehyve.nl>
2020-01-20 15:52:46 +0100

    Try to produce a system-dependent locale bug

--- tests/test_general.py
+++ tests/test_general.py
@@ -1,3 +1,7 @@
+import os
+from tempfile import NamedTemporaryFile
+import locale
+
 from opentargets_urlzsource import URLZSource
 
 
@@ -30,3 +34,21 @@ def test_file_url():
     with URLZSource('file://./tests/general.txt').open() as source:
         content = source.readlines()
         assert content == ["Foo"]
+
+
+def test_different_platform_encoding():
+    """Parse the input as UTF-8 regardless of platform encoding."""
+    f = NamedTemporaryFile(suffix='.txt', delete=False)
+    try:
+        f.write(b"F\xc3\xb8o\n")
+        f.close()
+        old_encoding_func = locale.getpreferredencoding
+        locale.getpreferredencoding = lambda: "latin_1"
+        try:
+            with URLZSource(f.name).open() as source:
+                content = source.readline()
+                assert content.encode("UTF-8") == b"F\xc3\xb8o\n"
+        finally:
+            locale.getpreferredencoding = old_encoding_func
+    finally:
+        os.remove(f.name)
